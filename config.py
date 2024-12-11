922e63ac615facafa1a6c0d03f37825b988f9fec
ivanstanley143 <66038840+ivanstanley143@users.noreply.github.com>
2024-12-11 22:05:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7587827209:AAE3BiNXrxwoGgIdUGWKhRvuJFm9rNukB0o")
 API_ID = int(os.environ.get("API_ID", "16795822"))
 API_HASH = os.environ.get("API_HASH", "791cc620cf2344a0fbf20b258fa679d2")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002297329742"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "1002297329742"))
 AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "7718122660 561339410").split())
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://damon:damon@cluster0.dxbn6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "Cluster0")
