c59352c3b88c63b4b07404b889a1f9c769b5e414
ivanstanley143 <66038840+ivanstanley143@users.noreply.github.com>
2024-12-11 11:13:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,4 @@ AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "7718122660 561339
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://ivanstanley143:<ivanstanley143>@cluster0.zi4xs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "broadcast")
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+https://graph.org/file/5ec5f5f926e1c5a31aa40-3e945218d176d4034f.jpg
